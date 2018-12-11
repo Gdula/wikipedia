96197609ded2ecb097078875720b6f4df0cc8449
Gdula <da.gdula@gmail.com>
2018-12-12 00:32:21 +0100

    Improve function choose language

--- app/wiki.py
+++ app/wiki.py
@@ -1,18 +1,42 @@
 import wikipedia
 import sys
+import os
 
 
-def choose_language(language):
-    wikipedia.set_lang(language)
+def choose_language():
+    user_input = input("[1]. Wpisz kod WP swojego języka(en, pl, de, etc.)\n[2]. Wybierz język z listy\n")
+    language_choosen = False
+    languages = wikipedia.languages()
+
+    if(user_input == '1'):
+        while not language_choosen:
+            wp_code = input("Wpisz kod:")
+            if wp_code in languages:
+                wikipedia.set_lang(wp_code)
+                language_choosen = True
+            else:
+                print("Nie istnieje taki kod!")
+    else:
+        show_languages(languages)
+        while not language_choosen:
+            search_language = input("Podaj język")
+            if search_language in languages.values():
+                for wp, lang in languages.items():
+                    if lang == search_language:
+                        wikipedia.set_lang(wp)
+                        language_choosen = True
+            else:
+                print("Nie ma takiego języka")
 
 
-def show_languages():
+def show_languages(languages):
     i = 1
     languages = wikipedia.languages()
 
     for key, value in languages.items():
         print("{}. {}".format(i, value))
         i += 1
+
 
 def search():
     quary = input("Co chcesz wyszukać?: ")
@@ -39,21 +63,20 @@ def print_main_menu():
 
 
 def main():
-    show_languages()
-    language = input("Wybierz język:")
-    choose_language(language)
+    choose_language()
 
-    print_main_menu()
-    user_input: str = input("Wybierz co chcesz zrobić: ")
+    while True:
+        print_main_menu()
+        user_input: str = input("Wybierz co chcesz zrobić: ")
 
-    if (user_input == '1'):
-        search()
-    elif (user_input == '2'):
-        summary()
-    elif (user_input == '2'):
-        suggest()
-    elif (user_input == '3'):
-        sys.exit()
+        if (user_input == '1'):
+            search()
+        elif (user_input == '2'):
+            summary()
+        elif (user_input == '2'):
+            suggest()
+        elif (user_input == '0'):
+            sys.exit()
 
 
 if __name__ == "__main__":
